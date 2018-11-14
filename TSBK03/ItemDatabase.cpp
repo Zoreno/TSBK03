48acccd518fb69258f30f767ef54b86124d6bdf9
Zoreno <metallica317@spray.se>
2018-11-14 23:58:18 +0100

    Some missing files

--- TSBK03/ItemDatabase.cpp
+++ TSBK03/ItemDatabase.cpp
@@ -2,7 +2,7 @@
 
 ItemDatabase::ItemDatabase()
 {
-	_items.emplace(0, new Item(0, "Fisk", ItemType::GENERIC, ItemRarity::EPIC, "\"This is a fish\"", 1, 0, 0));
+	_items.emplace(0, new Item(0, "Fisk", ItemType::GENERIC, ItemRarity::LEGENDARY, "\"This is a fish\"", 1, 0, 0));
 	_items.emplace(1, new Item(1, "Sten", ItemType::GENERIC, ItemRarity::SUPERIOR, "\"This is a sten\"", 1, 1, 0));
 }
 
